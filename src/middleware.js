745b167cd4e17b24969b62feaaca68f371b07005
Misachu10032 <johnzhou.engineer@gmail.com>
2025-01-14 15:16:53 -0700

    admin routes protection

--- src/middleware.js
+++ src/middleware.js
@@ -6,25 +6,25 @@ export async function middleware(request) {
   const token = request.cookies.get('token');
   
   // Define protected paths that require authentication
-  const protectedPaths = ['/home', '/admin'];
+  const protectedPaths = ['/home', '/admin', '/api/confirm-request'];
   // Define public paths that should redirect to home if already authenticated
   const publicPaths = ['/login', '/register'];
 
   const isProtectedRoute = protectedPaths.includes(request.nextUrl.pathname);
   const isPublicRoute = publicPaths.includes(request.nextUrl.pathname);
-  
+
   try {
     if (token) {
       const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_SECRET_KEY);
       const verified = await jose.jwtVerify(token.value, secret);
       const payload = verified.payload;
       
-      // Check if the user is accessing the admin route and has the 'Admin' role
-      if (isProtectedRoute && request.nextUrl.pathname === '/admin') {
-        if (payload.role !== 'Admin') {
-          // If the user does not have admin role, redirect to home or another appropriate page
-          return NextResponse.redirect(new URL('/home', request.url));
-        }
+      // Check if the user is trying to access a protected admin route
+      const isAdminRoute = ['/admin', '/api/confirm-request'].includes(request.nextUrl.pathname);
+
+      if (isProtectedRoute && isAdminRoute && payload.role !== 'Admin') {
+        // If the user does not have admin role, redirect to home
+        return NextResponse.redirect(new URL('/home', request.url));
       }
       
       if (isPublicRoute) {
@@ -54,6 +54,7 @@ export const config = {
   matcher: [
     '/home',
     '/admin',
+    '/api/confirm-request', // Protect the API path
     '/login',
     '/register'
   ]
