fbd9e43123993117090f6aad777fd79481d9fdd5
Misachu10032 <johnzhou.engineer@gmail.com>
2025-01-22 12:07:33 -0700

    chinese rules added

--- src/app/rules/page.js
+++ src/app/rules/page.js
@@ -1,6 +1,14 @@
 "use client";
 
+import { useState } from "react";
+
 const RulesPage = () => {
+  const [language, setLanguage] = useState("zh"); // Default language is English
+
+  const toggleLanguage = () => {
+    setLanguage((prevLang) => (prevLang === "en" ? "zh" : "en"));
+  };
+
   const handleNavigateToLogin = () => {
     window.location.href = "/login";
   };
@@ -8,72 +16,93 @@ const RulesPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl w-full bg-white rounded-xl shadow-2xl p-8 space-y-6">
-        <h1 className="text-4xl font-extrabold text-center text-indigo-600">
-          Rules
-        </h1>
+        <div className="flex justify-between items-center">
+          <h1 className="text-4xl font-extrabold text-indigo-600">
+            {language === "en" ? "Rules" : "规则"}
+          </h1>
+          <button
+            onClick={toggleLanguage}
+            className="px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700"
+          >
+            {language === "en" ? "中文" : "English"}
+          </button>
+        </div>
+
         <div className="text-gray-700 space-y-5">
-          <p className="text-lg font-medium text-center">I'm working on it</p>
+          <p className="text-lg font-medium text-center">
+            {language === "en" ? "I'm working on it" : "我还在肝"}
+          </p>
 
           <h2 className="text-2xl font-semibold text-gray-800 border-b-2 border-gray-200 pb-2">
-            Winning Team
+            {language === "en" ? "Winning Team" : "获胜队伍"}
           </h2>
-          <p className="text-lg">Score calculation:</p>
+          <p className="text-lg">
+            {language === "en" ? "Score calculation:" : "算分规则"}
+          </p>
           <p className="bg-gray-50 border-l-4 border-indigo-500 px-4 py-2 rounded-md">
-            (200 - gamesWonAgainstTheLosingTeam * 10) * decisiveMultiplier *
-            numberOfGamesPlayed
+            {language === "en"
+              ? "(200 - gamesWonAgainstTheLosingTeam * 10) * decisiveMultiplier * numberOfGamesPlayed"
+              : "（基础分-杀熟分）x胜负系数x赢的局数"}
           </p>
 
           <div className="bg-gray-50 border-l-4 border-indigo-500 px-4 py-2 rounded-md space-y-3">
-            <p className="text-base">Details:</p>
+            <p className="text-base">
+              {language === "en" ? "Details:" : "详细规则："}
+            </p>
             <ul className="list-disc pl-5">
-              <li className="text-base">
-                gamesWonAgainstTheLosingTeam is calculated for each player.
+              <li>
+                {language === "en"
+                  ? "gamesWonAgainstTheLosingTeam is calculated for each player."
+                  : "基础分100"}
               </li>
-              <>
-                Example: If A and B win against C and D, A's value includes all
-                wins against C and D.
-              </>
-              <>
-                If A won a double match against C and D, and a single against D,
-                A's value is 3.
-              </>
-
-              <li className="text-base">
-                {" "}
-                The decisiveMultiplier increases to 1.5 if the winning team's
-                score is more than twice the losing team's, otherwise it's 1.
+              <li>
+                {language === "en"
+                  ? "Example: If A and B win against C and D, A's value includes all wins against C and D."
+                  : "杀熟分：赢过对手的次数。"}
               </li>
-
-              <li className="text-base">
-                numberOfGamesPlayed: 1 for best of 1, 2 for best of 3, 3 for
-                best of 5.
+              <li>
+                {language === "en"
+                  ? "The decisiveMultiplier increases to 1.5 if the winning team's score is more than twice the losing team's, otherwise it's 1."
+                  : "（基础分-杀熟分）的下线是50"}
+              </li>
+              <li>
+                {language === "en"
+                  ? "numberOfGamesPlayed: 1 for best of 1, 2 for best of 3, 3 for best of 5."
+                  : "胜负系数： 若胜方分数是负方的两倍以上，胜负系数为1.5，否则为1。"}
               </li>
             </ul>
           </div>
 
-
-
           <h2 className="text-2xl font-semibold text-gray-800 border-b-2 border-gray-200 mt-6 pb-2">
-            Losing Team
+            {language === "en" ? "Losing Team" : "失败队伍"}
           </h2>
-          <p className="text-lg">Score calculation:</p>
+          <p className="text-lg">
+            {language === "en" ? "Score calculation:" : "算分规则："}
+          </p>
           <p className="bg-gray-50 border-l-4 border-red-500 px-4 py-2 rounded-md">
-            (hardworkScore + rankingDifferenceScore) * numberOfGamesPlayed
+            {language === "en"
+              ? "(hardworkScore + rankingDifferenceScore) * numberOfGamesPlayed"
+              : "（拼搏分+挑战分）x比赛局数"}
           </p>
           <div className="bg-gray-50 border-l-4 border-red-500 px-4 py-2 rounded-md space-y-3">
-            <p className="text-base">Details:</p>
+            <p className="text-base">
+              {language === "en" ? "Details:" : "详细规则："}
+            </p>
             <ul className="list-disc pl-5">
               <li>
-                hardworkScore: 50 if the total score ratio is less than 1.5,
-                otherwise 0.
+                {language === "en"
+                  ? "hardworkScore: 50 if the total score ratio is less than 1.5, otherwise 0."
+                  : "拼搏分：总比分差低于1.5时为20分，否则为0。"}
               </li>
               <li>
-                rankingDifferenceScore: 50 if the difference between combined
-                scores of teams exceeds 1000.
+                {language === "en"
+                  ? "rankingDifferenceScore: 50 if the difference between combined scores of teams exceeds 1000."
+                  : "挑战分：队伍排名分数总和差超过1000时得20分。"}
               </li>
-              <li className="text-base">
-                numberOfGamesPlayed: 1 for best of 1, 2 for best of 3, 3 for
-                best of 5.
+              <li>
+                {language === "en"
+                  ? "numberOfGamesPlayed: 1 for best of 1, 2 for best of 3, 3 for best of 5."
+                  : ""}
               </li>
             </ul>
           </div>
@@ -84,7 +113,7 @@ const RulesPage = () => {
             onClick={handleNavigateToLogin}
             className="inline-flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Go to Login
+            {language === "en" ? "Go to Login" : "前往登录"}
           </button>
         </div>
       </div>
